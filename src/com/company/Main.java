eb3fbcd7d46cd70a09fd0b54fac57f05d3024b56
DanielKaczorowski <email@daniel.com.pl>
2015-11-07 16:41:47 +0100

    k1

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=2;
+        int b=-3;
+        int c=-8;
+        int delta =b*b-4*a*c;
+        double odelta;
+                odelta=Math.sqrt(delta);
+        double x1=(-b-odelta)/2*a;
+        double x2=(-b+odelta)/2*a;
+        
+
+
+
+
+    }
+
+}
